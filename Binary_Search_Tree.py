dfb70d66de6840d5278b2a4b3f9f570cec414640
phanitallapudi <phanitallapudi@gmail.com>
2024-07-02 12:26:11 +0530

    add: added Binary_Search_Tree.py

---
+++ Binary_Search_Tree.py
@@ -0,0 +1,47 @@
+class Node(object):
+    def __init__(self, value) -> None:
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self) -> None:
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while True:
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+    def contains(self, value):
+        if self.root is None:
+            return False
+        temp = self.root
+        while True:
+            if value == temp.value:
+                return True
+            if value < temp.value:
+                if temp.left is None:
+                    return False
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    return False
+                temp = temp.right
+        return False
+
