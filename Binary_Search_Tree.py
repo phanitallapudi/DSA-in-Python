a2719acdbf9c21014ee85c59ee2023b0f9e61772
phanitallapudi <phanitallapudi@gmail.com>
2024-07-03 11:13:48 +0530

    update: updated Binary_Search_Tree.py to include minumum value of all node using recursion and list traversion

--- Binary_Search_Tree.py
+++ Binary_Search_Tree.py
@@ -87,3 +87,34 @@ class BinarySearchTree:
     
     def reverse(self):
         return self.reverse_node(self.root)
+    
+    def min_node(self, node):
+        if node is None:
+            return float("+inf")
+        leftMin = self.min_node(node.left)
+        rightMin = self.min_node(node.right)
+        return min(node.value, leftMin, rightMin)
+    
+    def min_traversal(self):
+        temp = self.root
+        if temp is None:
+            return None
+        
+        left_min = temp.value
+        left_temp = temp
+        while left_temp is not None:
+            if left_temp.value < left_min:
+                left_min = left_temp.value
+            left_temp = left_temp.left
+        
+        right_min = temp.value
+        right_temp = temp
+        while right_temp is not None:
+            if right_temp.value < right_min:
+                right_min = right_temp.value
+            right_temp = right_temp.right
+
+        return min(left_min, right_min)
+    
+    def min_recursion(self):
+        return self.min_node(self.root)
