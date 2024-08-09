b9f5b5bbd1185b4ae4ff4e1b8a054a0c7c59fe1e
phanitallapudi <phanitallapudi@gmail.com>
2024-08-09 10:10:40 +0530

    add: added Bubble_Sort.py

---
+++ Bubble_Sort.py
@@ -0,0 +1,9 @@
+def sort(items):
+    for i in range(len(items)):
+        for j in range(len(items) - 1):
+            if items[i] < items[j]:
+                items[i], items[j] = items[j], items[i]
+    return items
+
+items = [1, 5, 3, 2, 4]
+print(sort(items))
