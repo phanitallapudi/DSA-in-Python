6bd49fd00ef2bd6e47c4f909f957ade83f7b8153
phanitallapudi <phanitallapudi@gmail.com>
2024-08-09 10:33:47 +0530

    add: added Selection_Sort.py

---
+++ Selection_Sort.py
@@ -0,0 +1,12 @@
+def sort(items):
+    n = len(items)
+    for i in range(n):
+        min_index = i
+        for j in range(i+1, n):
+            if items[j] < items[min_index]:
+                min_index = j
+        items[i], items[min_index] = items[min_index], items[i]
+    return items
+
+items = [4, 5, 2, 3, 1]
+print(sort(items))
