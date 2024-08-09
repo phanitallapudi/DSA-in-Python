1974e8042fba0c67dd8ab672ffd102d9c310e350
phanitallapudi <phanitallapudi@gmail.com>
2024-08-09 10:45:45 +0530

    update: updated the Selection_Sort.py to optimized method

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -5,7 +5,8 @@ def sort(items):
         for j in range(i+1, n):
             if items[j] < items[min_index]:
                 min_index = j
-        items[i], items[min_index] = items[min_index], items[i]
+        if i != min_index:
+            items[i], items[min_index] = items[min_index], items[i]
     return items
 
 items = [4, 5, 2, 3, 1]
